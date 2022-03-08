22928398dc31519cf6324ef748d2bcc8eec819d4
Sergio Jiménez <sergio.jimenez@epn.edu.ec>
2022-03-07 19:11:50 -0500

    Correccion de interfaces

--- app/src/main/java/com/example/standbyme/EscogerUsuario.kt
+++ app/src/main/java/com/example/standbyme/EscogerUsuario.kt
@@ -1,11 +1,32 @@
 package com.example.standbyme
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class EscogerUsuario : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_escoger_usuario)
+
+        val ivAdultoMayor = findViewById<ImageView>(R.id.iv_adulto_mayor)
+        val ivEncargado = findViewById<ImageView>(R.id.iv_encargado)
+
+        ivAdultoMayor
+            .setOnClickListener{
+                //irActividad(EscogerUsuario::class.java)
+            }
+
+        ivEncargado
+            .setOnClickListener{
+                //irActividad(EscogerUsuario::class.java)
+            }
+    }
+
+    fun irActividad(clase: Class<*>){
+        val intent = Intent(this, clase)
+        startActivityForResult(intent, 0)
     }
 }
--- app/src/main/java/com/example/standbyme/MainActivity.kt
+++ app/src/main/java/com/example/standbyme/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.standbyme
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val ivInicio = findViewById<TextView>(R.id.iv_inicio)
+        ivInicio
+            .setOnClickListener{
+                irActividad(EscogerUsuario::class.java)
+            }
     }
+
+    fun irActividad(clase: Class<*>){
+        val intent = Intent(this, clase)
+        startActivityForResult(intent, 0)
+    }
+
 }
